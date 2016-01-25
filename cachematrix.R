0cad78330678afc692c4e3c54883fd8d257de464
Brandon Koch <brandonkoch@KOCH-MACBOOKPRO.local>
2016-01-24 18:15:24 -0700

    Additional comments and clarification for code along with test solution

--- cachematrix.R
+++ cachematrix.R
@@ -5,15 +5,26 @@
 ## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  ## Set the cached_matrix to NULL
+  cached_matrix <- NULL
+  
+  ## Set function for cached_matrix
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    cached_matrix <<- NULL
   }
+  
+  ## Get function for cached_matrix
   get <- function() x
-  setinverse <- function(inverse) m <<- inverse
-  getinverse <- function() m
-  list(set = set, get = get, setinverse = setinverse, 
+  
+  ## Setinverse function for cached_matrix
+  setinverse <- function(inverse) cached_matrix <<- inverse
+  
+  ## Getinverse function for cached_matrix
+  getinverse <- function() cached_matrix
+  
+  list(set = set, get = get, 
+       setinverse = setinverse, 
        getinverse = getinverse)
 }
 
@@ -25,13 +36,28 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cache.
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
-  if(!is.null(m)) {
+  ## Get the inverse if it exists
+  inverted_matrix <- x$getinverse()
+  if(!is.null(inverted_matrix)) {
     message("getting cached data")
-    return(m)
+    ## If it is not null, return the cached matrix
+    return(inverted_matrix)
   }
-  data <- x$get()
-  m <- solve(data) %*% data
-  x$setinverse(m)
-  m
+  ## Otherwise, get the matrix
+  matrix_toinvert <- x$get()
+  ## Invert the matrix
+  inverted_matrix <- solve(matrix_toinvert)
+  ## Save the inverted_matrix to the cache
+  x$setinverse(inverted_matrix)
+  ## Return the inverted_matrix
+  inverted_matrix
 }
+
+## You may test the functions with the code below:
+## test_matrix <- cbind(c(4,2),c(7,6))
+## cached_matrix <- makeCacheMatrix(test_matrix)
+## cacheSolve(cached_matrix)
+##Solution:
+##      [,1] [,2]
+##[1,]  0.6 -0.7
+##[2,] -0.2  0.4
